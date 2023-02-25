d37bd6296d8b873e9e789074c35dc4484f1d7e7b
allixina <125519965+allixina@users.noreply.github.com>
2023-02-25 19:42:37 +0700

    Update script.js
    Delete Function LOGIN

--- script.js
+++ script.js
@@ -48,16 +48,3 @@ document.querySelector('#submit').addEventListener('focus', function(e) {
   });
 });
 
-function login() {
-    let username = document.getElementById('formlogin').username.value;
-    let password = document.getElementById('formlogin').password.value;
-    let Result = document.getElementById('wrong');
-
-    if (username == 'xrpl1' && password == '123') {
-        window.open("homepage.html", "_self");
-    } else if (username == 'user1' && password == '123') {
-        window.open("homepage.html", "_self");
-    } else {
-        Result.innerHTML = 'Username atau Password salah';
-    }
-}
